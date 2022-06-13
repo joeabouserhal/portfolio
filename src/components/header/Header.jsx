c1d00339429c8748737516be3876f800cd6bf544
Joe Abou Serhal <80884927+joeabouserhal@users.noreply.github.com>
2022-06-13 11:01:37 +0300

    added navbar to mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GrMenu } from 'react-icons/gr'
-import {FiMenu} from 'react-icons/fi'
+import { FiMenu, FiX } from 'react-icons/fi'
 
 var navBarItemStyle = 'navbar-item flex text-md font-mono p-0.5 text-white cursor-pointer hover:text-slate-500 transition hover:scale-125';
+var mobileNavBarItemStyle = 'navbar-item flex text-md font-mono p-0.5 text-white';
+
 
 const Header = () => {
+
+    const [navBarOpen, setNavBarOpen] = useState(false);
+
     return (
         <header className='header'>
-            <nav className='navbar flex p-4 gap-20 '>
+            <nav className='relative navbar flex p-4 gap-20 '>
                 <div className='title'>
                     <p className='text-xl font-mono text-white'>Portfolio.</p>
                 </div>
@@ -19,7 +24,15 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className='flex justify-end'>
-                    <FiMenu size='2rem' color='white' className=' visible md:invisible lg:invisible absolute right-5 cursor-pointer' />
+                    {navBarOpen ?
+                        <FiX size='2rem' color='white' className=' visible md:invisible lg:invisible absolute right-5 cursor-pointer' onClick={() => setNavBarOpen(!navBarOpen)} /> :
+                        <FiMenu size='2rem' color='white' className=' visible md:invisible lg:invisible absolute right-5 cursor-pointer' onClick={() => setNavBarOpen(!navBarOpen)} />}
+                </div>
+                <div className={(navBarOpen ? 'justify-center items-center pt-10' : 'hidden')}>
+                    <ul className='flex flex-col gap-2 items-center justify-center self-center'>
+                        <li /><a onClick={scrollToAboutMe} className={mobileNavBarItemStyle+'justify-center text-center'}>About Me.</a>
+                        <li /><a className={mobileNavBarItemStyle+'justify-center text-center'}>Contact Me.</a>
+                    </ul>
                 </div>
             </nav>
         </header>
@@ -28,6 +41,6 @@ const Header = () => {
 
 function scrollToAboutMe() {
     const elementToView = document.getElementById("aboutMe");
-    elementToView.scrollIntoView(); 
-    }
+    elementToView.scrollIntoView();
+}
 export default Header
