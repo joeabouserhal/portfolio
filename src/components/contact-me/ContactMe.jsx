ca2b36cd4a260bbd3e1d7c6e61b9a85119f2db9c
Joe Abou Serhal <80884927+joeabouserhal@users.noreply.github.com>
2022-06-20 22:19:33 +0300

    added contact me title

--- src/components/contact-me/ContactMe.jsx
+++ src/components/contact-me/ContactMe.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const ContactMe = () => {
     return (
-        <div id="contactMe">Contact Me</div>
+        <div id="contactMe">
+            <div className="justify-center">
+                <h1 className="title text-3xl font-mono text-center">Contact Me</h1>
+            </div>
+        </div>
     )
 }
 
