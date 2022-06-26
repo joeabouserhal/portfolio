547604fdacebf24f5456f2bdf0f24e12a49c9031
joeabouserhal <joeabouserhal77777@gmail.com>
2022-06-26 12:23:36 +0300

    fixed contact me section scaling

--- src/components/contact-me/ContactMe.jsx
+++ src/components/contact-me/ContactMe.jsx
@@ -8,15 +8,15 @@ const ContactMe = () => {
             <div className="title">
                 <h1 className="title text-3xl">Contact Me</h1>
             </div>
-            <div className="flex pt-16 justify-around">
+            <div className="flex pt-16 justify-center gap-20">
                 <div>
-                    <p className="text-md p-5">Send me an Email</p>
+                    <p className="text-md p-5 lg:w-64">Send me an Email</p>
                     <a className="flex align-middle justify-center" target="_blank" href="mailto:joeabouserhal77777@gmail.com">
                         <FiMail size="2.5rem" />
                     </a>
                 </div>
                 <div>
-                    <p className="text-md p-5">Talk to me on Instagram</p>
+                    <p className="text-md p-5 lg:w-64">Talk to me on Instagram</p>
                     <a className="flex align-middle justify-center" target="_blank" href="https://www.instagram.com/joe.abou.serhal/">
                         <BsInstagram size="2rem" />
                     </a>
